517355a7fc0927df5a6b06179036d6d23a7f99e5
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-06 17:24:05 +0530

    added an new program

--- Arrays/SumAtEvenPosition.java
+++ Arrays/SumAtEvenPosition.java
@@ -1,5 +1,18 @@
 package Arrays;
 
-public class SumAtEvenPosition {
+import java.util.Scanner;
 
+public class SumAtEvenPosition {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int[] arr= {1,2,3,4,5,6,7,8};
+	int sum=0;
+	for(int i=0;i<arr.length;i++) {
+		if(i%2==0) {
+			sum=sum+arr[i];
+		}
+	}
+	System.out.println(sum);
+	sc.close();
+}
 }
