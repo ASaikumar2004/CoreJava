8b12f11841fdb2a5e2593852cdd78534fb44da86
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-07 10:10:26 +0530

    Added some new program iin arrays

--- Arrays/MultiDA.java
+++ Arrays/MultiDA.java
@@ -1,9 +1,23 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class MultiDA {
 
 	public static void main(String[] args) {
-		int arr[][]= {{1,2,3},{4,5,6},{7,8,9}};
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the rows");
+		int row=sc.nextInt();
+		System.out.println("Enter the columns:");
+		int col=sc.nextInt();
+		int arr[][]= new int[row][col];
+		System.out.println("Enter the elements: ");
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[i].length;j++) {
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		
 		for(int i=0;i<arr.length;i++) {
 			for(int j=0;j<arr[i].length;j++) {
 				System.out.print(arr[i][j]+" ");
--- Arrays/SecondHighest.java
+++ Arrays/SecondHighest.java
@@ -3,15 +3,15 @@ package Arrays;
 public class SecondHighest {
 
 	public static void main(String[] args) {
-		int[] arr= {3,4,6,7,1,2,8};
+		int[] arr= {8,3,4,6,7,1,2,8};
 		int max=arr[0];
 		int max2=arr[0];
 		for(int i=0;i<arr.length;i++) {
-			if(arr[i] > max) {
+			if(arr[i] > max || max==max2) {
 				max2=max;
 				max=arr[i];
 			}
-			else if(arr[i]>max2 && arr[i]!=max) {
+			else if(arr[i]>max2 && arr[i]<max) {
 				max2=arr[i];
 			}
 		}
