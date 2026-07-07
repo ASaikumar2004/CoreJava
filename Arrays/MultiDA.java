8020fe01032b2aef05eb42c5f7fde04d6b75bc0c
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-07 10:13:39 +0530

    Added some new program iin arrays

--- Arrays/MultiDA.java
+++ Arrays/MultiDA.java
@@ -20,7 +20,9 @@ public class MultiDA {
 		
 		for(int i=0;i<arr.length;i++) {
 			for(int j=0;j<arr[i].length;j++) {
+				if(arr[i][j]%2==0) {
 				System.out.print(arr[i][j]+" ");
+				}
 			}
 			System.out.println();
 		}
