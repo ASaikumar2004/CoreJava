ab5babb0220207c138211b260712ba658c8c45fd
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-09 09:58:15 +0530

    added new files

--- PatternPrinting/NumberLeftAngle.java
+++ PatternPrinting/NumberLeftAngle.java
@@ -1,18 +1,18 @@
 package PatternPrinting;
 
 public class NumberLeftAngle {
-public static void main(String[] args) {
-	int n=4;
-	/*
-	 * for(int i=1;i<=n;i++) { for(int j=1;j<=i;j++) { System.out.print(j); }
-	 * System.out.println(); }
-	 */
-	
-	for(int i=n;i>=1;i--) {
-		for(int j=1;j<=i;j++) {
-			System.out.print(j);
+	public static void main(String[] args) {
+		int n = 4;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
 		}
-		System.out.println();
+
+		/*
+		 * for(int i=n;i>=1;i--) { for(int j=1;j<=i;j++) { System.out.print(j); }
+		 * System.out.println(); }
+		 */
 	}
-}
 }
