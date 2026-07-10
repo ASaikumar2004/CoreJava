3fc6ec97530725d653a300b207adf96eb5b0cf6c
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-10 09:23:05 +0530

    Addednew patterns

--- Arrays/MinimumNumber.java
+++ Arrays/MinimumNumber.java
@@ -2,7 +2,7 @@ package Arrays;
 
 public class MinimumNumber {
 public static void main(String[] args) {
-	int[] arr= {5,7,9,4,2,4,5,87};
+	int[] arr= {5,7,9,4,2,4,5,87,1};
 	int min=arr[0];
 	for(int i=0;i<arr.length;i++) {
 		if(min>arr[i]) {
--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -6,7 +6,9 @@ public class ReverseArray {
 		int[] arr= {10,20,30,40};
 		for(int i=arr.length-1;i>=0;i--) {
 			System.out.print(arr[i]+" ");
+			
 		}
+		System.out.println();
 		
 //		Accessing last element;
 		System.out.println(arr[arr.length-1]);
