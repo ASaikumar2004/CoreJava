78da2969586a95c6c400e2af44a1a289a8f4e48d
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-16 10:43:26 +0530

    modified file in oops concept

---
+++ Methods/demo1.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class demo1 {
+	
+	public void add(int a,int b) {
+		int sum=a+b;
+		System.out.println("sum is: "+sum);
+	}
+public static void main(String[] args) {
+	demo1 d=new demo1();
+	d.add(10,20);
+}	
+}
