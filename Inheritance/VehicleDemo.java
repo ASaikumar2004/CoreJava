555cbfa1d65c29a78bb39714088ba2fa2647fa75
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-23 10:23:15 +0530

    Inheritance basic problems

---
+++ Inheritance/VehicleDemo.java
@@ -0,0 +1,42 @@
+package Inheritance;
+
+class vehicle {
+
+	void sunroof() {
+		System.out.println("Vehicle opened sunroof");
+	}
+
+	void wheels() {
+		System.out.println("Its a 4 wheeler vehicle");
+	}
+}
+
+class car extends vehicle {
+	public void car1() {
+		System.out.println("vehicle is car");
+	}
+}
+
+class electriccar extends car{
+	public void electric() {
+		System.out.println("Vehicle is electric car");
+	}
+}
+
+public class VehicleDemo {
+
+	public static void main(String[] args) {
+		/*
+		 * car c = new car(); c.sunroof(); c.wheels();
+		 */
+		
+		electriccar ec=new electriccar();
+		ec.electric();
+		ec.car1();
+		ec.sunroof();
+		ec.wheels();
+		
+
+	}
+
+}
