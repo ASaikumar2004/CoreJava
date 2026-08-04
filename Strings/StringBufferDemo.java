9991535c9a71a7a5840b8d9f9ed00a853a908370
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-08-04 09:52:08 +0530

    added a Strings

--- Strings/StringBufferDemo.java
+++ Strings/StringBufferDemo.java
@@ -2,10 +2,10 @@ package Strings;
 
 public class StringBufferDemo {
 	public static void main(String[] args) {
-		StringBuffer sb=new StringBuffer("java");
-//		System.out.println(sb);
+		//StringBuffer sb=new StringBuffer("java");
+        //System.out.println(sb);
 		//sb.append("developer");
-		System.out.println(sb);
+		//System.out.println(sb);
 		
 		//reverse
 		//System.out.println(sb.reverse());
@@ -14,13 +14,22 @@ public class StringBufferDemo {
 		//System.out.println(sb.length());
 		
 		//insert(0,"core");
-		System.out.println(sb.insert(0, "Core"));
+		//System.out.println(sb.insert(0, "Core"));
 		
 		//capacity()
-		StringBuffer sb1=new StringBuffer();
-		System.out.println(sb1.length()); //0
-		System.out.println(sb1.capacity()); //16
+		//StringBuffer sb1=new StringBuffer();
+		//System.out.println(sb1.length()); //0
+		//System.out.println(sb1.capacity()); //16
 		
+		//delete
+		StringBuffer sb1=new StringBuffer("core java");
+		System.out.println(sb1);
+		//System.out.println(sb1.delete(2, 5));
+		//System.out.println(sb1.deleteCharAt(4));
+		
+		//setCharAt();
+		sb1.setCharAt(1, 'k');
+		System.out.println(sb1);
 		
 		
 //		String s="java";
