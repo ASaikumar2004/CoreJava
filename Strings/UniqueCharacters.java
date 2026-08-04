ec7ad30d7ee82108cef3a53284815cc9a7f7795e
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-08-04 11:10:30 +0530

    added a Strings

---
+++ Strings/UniqueCharacters.java
@@ -0,0 +1,20 @@
+package Strings;
+
+public class UniqueCharacters {
+	public static void main(String[] args) {
+		String str = "javajavajavadevdevdev";
+		char ch[] = str.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			int count = 0;
+			for (int j = 0; j < i; j++) {
+				if (ch[i] == ch[j]) {
+					count++;
+				}
+			}
+			if (count == 1) {
+				System.out.print(ch[i] + " ");
+			}
+		}
+	}
+
+}
