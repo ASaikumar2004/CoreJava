934946b72d6239488daf28b3511c2869943473b1
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-08-13 10:11:00 +0530

    added a new topic from collections framework

--- list/ArrayListDemo.java
+++ list/ArrayListDemo.java
@@ -8,27 +8,24 @@ public class ArrayListDemo {
 
 	public static void main(String[] args) {
 		List<String> al = new ArrayList<>();
+		
 
 		al.add("sai");
 		al.add("Sivani");
 		al.add("love");
-
+		
+		al.remove(0);
+		
 		System.out.println(al);
-		for (int i = 0; i < al.size(); i++) {
-			System.out.println(al.get(i));
-		}
-		
-		System.out.println("usinmg enhanced for loop");
-		System.out.println("=================");
-		for(String s:al) {
-			System.out.println(s);
-		}
-		
 		
 		Iterator<String> it=al.iterator();
 		while(it.hasNext()) {
-			System.out.println(it.next());
+			if(it.next().length()==4) {
+				it.remove();
+			}
 		}
+		
+		System.out.println(al);
 
 	}
 }
